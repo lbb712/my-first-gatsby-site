4b86043a15791fa8c900b18acfb4fd4787f7b4f0
Lily <lbreesetovey@gmail.com>
2021-07-02 10:13:02 +0100

    Added images to site pages usingn plugins

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,17 @@
 // Step 1: Import your component
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm Lily, the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="Close up picture of Lily, the author of this webpage. She has long red/brown hair and is wearing glasses. "
+        src="../images/lily.jpg"
+      />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
     <p>I'm making this by following the Gatsby Tutorial.</p>
+    <StaticImage
+      alt="Image of code on a computer screen"
+      src="https://www.signal-chief.com/wp-content/uploads/2018/04/Computer-Code.jpeg"
+    />
     </Layout>
   )
 }
